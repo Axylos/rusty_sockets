52d62b03205e392df2b530416729ad00f5b68bc4
Drake Talley <robertdraketalley@gmail.com>
2014-12-30 01:00:50 +0000

    print out from client
    Signed-off-by: Drake Talley <robertdraketalley@gmail.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,7 +4,10 @@ fn main() {
     let host = "ec2-54-148-208-119.us-west-2.compute.amazonaws.com:8080";
     let port = 8080u16;
     let mut socket = TcpStream::connect(host);
+    println!("connecting to server");
     socket.write(b"Get / HTTP/1.0\n\n");
+    println!("writing to server");
     let response = socket.read_to_end();
-    //comment
+    println!("response: \n{}\n", response);
+    println!("\n done");
 }
