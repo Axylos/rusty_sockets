63b76b7d957f9db140d61c0b6054dbfa41280f14
Drake Talley <robertdraketalley@gmail.com>
2015-01-05 04:23:10 +0000

    add parser to client now need to extract into module and add to server
    Signed-off-by: Drake Talley <robertdraketalley@gmail.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,32 @@
+extern crate getopts;
 use std::io::TcpStream;
+use getopts::{optopt, optflag, getopts, OptGroup, usage};
+use std::os;
 
 fn main() {
-    let host = "ec2-54-148-208-119.us-west-2.compute.amazonaws.com:8080";
-    let port = 8080u16;
-    let mut socket = TcpStream::connect(host);
+    let args: Vec<String> = os::args();
+    
+    let opts = &[
+        optopt("s", "", "server", "connect to this server host"),
+        optopt("p", "", "port", "connect to this host port")
+    ];
+
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let host = match matches.opt_present("s") {
+        true => matches.opt_str("s").unwrap(),
+        false => "ec2-54-148-208-119.us-west-2.compute.amazonaws.com".to_string(),
+    };
+
+    let port = match matches.opt_present("p") {
+        true => (matches.opt_str("p")).unwrap().parse().unwrap(),
+        false => 80u16,
+    };
+
+    let mut socket = TcpStream::connect((host.as_slice(), port));
     println!("connecting to server");
     let msg = b"would you like to play a game?\n\n";
     socket.write(msg);
